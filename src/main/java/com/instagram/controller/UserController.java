df8cb87e8fc3b161117c04087405e513fa619e74
Anmol Verma <vermaanmol1223@gmail.com>
2025-12-14 18:40:38 +0530

    Add UserController

---
+++ src/main/java/com/instagram/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.instagram.controller;
+
+import com.instagram.model.User;
+import com.instagram.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+}
