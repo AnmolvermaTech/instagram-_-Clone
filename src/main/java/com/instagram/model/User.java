d341dfe758ef6c0d2fc9e7aed56c15fba5c2ccd3
Anmol Verma <vermaanmol1223@gmail.com>
2025-12-14 18:33:03 +0530

    Add User entity

--- src/main/java/com/instagram/model/User.java
+++ src/main/java/com/instagram/model/User.java
@@ -15,6 +15,8 @@ public class User {
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
+
+    
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
 
